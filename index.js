428117c1863dc4b1a29f4c3292180534a5c01d48
StreamingMoon <105146006+StreamingMoon@users.noreply.github.com>
2024-02-06 19:32:13 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function MyPage()
 function Detail()
 {
     document.body.innerHTML = "";
-    document.write('<!DOCTYPE html><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="stylesheet" type="text/css" href="./index.css"><title>StreamingMoon\'s Page</title></head><body bgcolor="#010409"><div align="center"><img id="Avatar" src="./HoshinoAi_Eye.jpg" /></div><div align="center"><h1>StreamingMoon</h1></div><div align="center">A human.</div><div align="center"><button onclick="Back()">GoBack</button></div></body><script type="text/javascript" src="./index.js"></script></html>');
+    document.write('<!DOCTYPE html><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="stylesheet" type="text/css" href="./index.css"><title>StreamingMoon\'s Page</title></head><body bgcolor="#010409"><div align="center"><img id="Avatar" src="./HoshinoAi_Eye.jpg" /></div><div align="center"><h1>StreamingMoon</h1></div><div align="center">A human.</div><div align="center"><button onclick="Back()">Go Back</button></div></body><script type="text/javascript" src="./index.js"></script></html>');
 }
 
 function Back()
